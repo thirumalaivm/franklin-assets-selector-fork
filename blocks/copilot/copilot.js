1e09bb47b1d9e388b6dbdf008321ecd196bc5cc3
Satya Deep Maheshwari <satyam@adobe.com>
2023-05-24 19:31:52 +0530

    Added clipboard support

--- blocks/copilot/copilot.js
+++ blocks/copilot/copilot.js
@@ -39,6 +39,27 @@ export default function decorate(block) {
     const apiKeyInput = document.getElementById('apiKeyInput').value;
     const tokenInput = document.getElementById('tokenInput').value;
     const content = await generate(apiKeyInput, tokenInput, topic, item_count);
+    const table = `
+    <table border="1">
+    <tr>
+       <td colspan="2" style="background-color: #ff8012; color: #ffffff;  height:23px;">${block}</td>
+    </tr>
+    ${content.map((item) => {
+      return `
+      <tr>
+        <td>${item.text}</td>
+        <td>
+              <img loading="lazy" alt="" type="image/jpeg" src=${item.image} width="200" height="300">
+        </td>
+      </tr>
+      `;
+    }).join('')};
+    </table>
+    `;
+    const blob = new Blob([table], { type: 'text/html' });
+    const data = [new ClipboardItem({ [blob.type]: blob })];
+    navigator.clipboard.write(data);
+    window.parent.document.getElementById('hlx-sk-palette-copilot').classList.add('hlx-sk-hidden');
     console.log(content);
   });
 

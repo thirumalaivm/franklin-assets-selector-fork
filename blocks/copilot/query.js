7dd7bc4b5c3f69a2b43cf1699b517bd303e2560e
Varun Nagpal <vnagpal@adobe.com>
2023-05-26 19:08:51 +0530

    unique responses

--- blocks/copilot/query.js
+++ blocks/copilot/query.js
@@ -1,15 +1,24 @@
 	import {callAPI} from "./firefall.js";
 
 	const CREATE_QUERY_ENDPOINT = '/v2/query';
+	
+	var queries = [
+		"tell me something about {topic} in 50 words or less",
+		"tell me something new about {topic} in 50 words or less",
+		"tell me something different about {topic} in 50 words or less"
+	]
+	
+	var counter = 0;
 
 	async function getCard(conversation_id, topic, items, cfg) {
 		const body = {
 			"conversation_id": conversation_id,
 			"dialogue":{
-				"question": "Tell something new about " + topic
+				"question": queries[counter%3].replaceAll("{topic}", topic)
 				//"I need a JSON cards block on the topic " + topic + " which has " +  items + " items"
 			}
 		};
+		counter++;
 		return await callAPI(CREATE_QUERY_ENDPOINT, body, cfg);
 	}
 

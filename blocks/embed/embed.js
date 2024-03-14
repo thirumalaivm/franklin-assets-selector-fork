63c3411dcb013d688d956fbf25a874fdc942f8a9
Ritwik Srivastava <ritwik@Ritwiks-MacBook-Pro-2.local>
2024-03-14 13:25:30 +0530

    Add changes for embed block console error

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -89,7 +89,7 @@ const loadEmbed = (block, link, autoplay) => {
 
 export default function decorate(block) {
   const placeholder = block.querySelector('picture');
-  const link = block.querySelector('a').href;
+  const link = block.querySelector('a') ? block.querySelector('a').href : 'no-link';
   block.textContent = '';
 
   if (placeholder) {

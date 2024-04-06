d8f575e8aca82c03c46681ef8252dc19443253db
Satya Deep Maheshwari <satyam@adobe.com>
2024-04-06 23:29:43 +0530

    Add assets usage report

--- tools/asset-report/report.js
+++ tools/asset-report/report.js
@@ -5,26 +5,49 @@ var cardsContainer = document.getElementById('cardsContainer');
 fetch('/assets-index.json')
     .then(response => response.json())
     .then(data => {
-        // Extract the array of URLs
-        const urls = JSON.parse(data.data[0]['aem-assets']);
+        const rows = data.data;
+        const processedUrls = [];
+        rows.forEach(row => {
+            // Extract the array of URLs
+            const urls = JSON.parse(row['aem-assets']);
 
-        // Create cards for each URL
-        urls.forEach(url => {
-            const card = document.createElement('div');
-            card.classList.add('card');
+            // Create cards for each URL
+            urls.forEach(url => {
+                if (!processedUrls.includes(url)) {
+                    let type;;
+                    if (url.includes('/adobe/assets/urn:aaid:')) {
+                        type = 'Polaris';
+                    } else if (url.includes('/adobe/dynamicmedia/deliver/urn:aaid:')) {
+                        type = 'Dynamic Media - Next';
+                    } else if (url.includes('scene7.com/is/image/')) {
+                        type = 'Dynamic Media - Classic';
+                    }
 
-            const iframe = document.createElement('iframe');
-            iframe.src = url;
-            iframe.frameBorder = '0';
-            iframe.width = '100%';
-            iframe.height = '400px';
+                    if (type) {
+                        const card = document.createElement('div');
+                        card.classList.add('card');
 
-            card.appendChild(iframe);
-            const link = document.createElement('a');
-            link.href = url;
-            link.textContent = url;
-            card.appendChild(link);
-            cardsContainer.appendChild(card);
+                        const iframe = document.createElement('iframe');
+                        iframe.src = url;
+                        iframe.frameBorder = '0';
+                        iframe.width = '100%';
+                        iframe.height = '400px';
+
+                        card.appendChild(iframe);
+                        const link = document.createElement('a');
+                        link.href = url;
+                        link.textContent = 'Delivery Url';
+                        card.appendChild(link);
+
+                        const p = document.createElement('p');
+                        p.textContent = `Type: ${type}` ;
+                        card.appendChild(p);
+                        cardsContainer.appendChild(card);
+
+                    }
+                    processedUrls.push(url);
+                }
+            });
         });
     })
     .catch(error => {

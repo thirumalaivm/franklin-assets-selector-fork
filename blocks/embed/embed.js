36ec21af444d47d0809f2188f2bc383d768e7afa
Yugandhar Yashwantrao Kamdi <yugandharya@adobe.com>
2024-04-25 13:49:07 +0530

    Fix carousel indicators and add selective mobile view for embed block

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -16,7 +16,7 @@ const loadScript = (url, callback, type) => {
   return script;
 };
 
-const getDefaultEmbed = (url) => `<div class="embed-default">
+const getDefaultEmbed = (url, hasMobileView) => `<div class="embed-default ${hasMobileView ? " mobile-view" : ""}">
         <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
           scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
         </iframe>
@@ -81,7 +81,8 @@ const loadEmbed = (block, link, autoplay) => {
     block.innerHTML = config.embed(url, autoplay);
     block.classList = `block embed embed-${config.match[0]}`;
   } else {
-    block.innerHTML = getDefaultEmbed(url);
+    let hasMobileView = block.classList.contains('mobile-view');
+    block.innerHTML = getDefaultEmbed(url, hasMobileView);
     block.classList = 'block embed';
   }
   block.classList.add('embed-is-loaded');

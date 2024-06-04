081a14fe83614c5c6394f2ab4bf1014cea98b3c4
Yugandhar Yashwantrao Kamdi <yugandharya@adobe.com>
2024-06-05 02:55:42 +0530

    Add custom button support

--- scripts/video.js
+++ scripts/video.js
@@ -20,22 +20,59 @@ function loadVideoJs() {
   return promise;
 }
 
+function parseButtonConfig(type, configElement) {
+  const configs = configElement.querySelectorAll(':scope > li');
+  const configObj = [...configs].reduce((obj, cf) => {
+    const text = cf.textContent;
+    if (text.includes('|')) {
+      const parts = text.split('|');
+      const key = parts[0].trim().toLowerCase();
+      const value = parts[1].trim().toLowerCase();
+
+      if (key === 'position') {
+        obj.position = value;
+      }
+
+      if (key === 'svg') {
+        obj.svg = value;
+      }
+    }
+
+    return obj;
+  }, {});
+
+  return {
+    type,
+    ...configObj,
+  };
+}
+
 function parseConfig(block) {
   const urlElement = block.querySelector(':scope > p > a');
 
   const configs = block.querySelectorAll(':scope > ul > li');
-  const configObj = [...configs].reduce((obj, configElement) => {
-    if (configElement.childNodes.length === 1) {
-      const text = configElement.textContent;
-      if (text.includes(':')) {
-        const parts = text.split(':');
-        if (parts[0].trim().toLowerCase() === 'autoplay') {
-          obj.autoplay = parts[1].trim().toLowerCase() === 'on';
-        }
 
-        if (parts[0].trim().toLowerCase() === 'autopause') {
-          obj.autopause = parts[1].trim().toLowerCase() === 'on';
-        }
+  const configObj = [...configs].reduce((obj, configElement) => {
+    const text = configElement.childNodes.length === 1
+      ? configElement.textContent
+      : configElement.firstChild.textContent;
+
+    if (text.includes('|')) {
+      const parts = text.split('|');
+      const key = parts[0].trim().toLowerCase();
+      const value = parts[1].trim().toLowerCase();
+
+      if (key === 'autoplay') {
+        obj.autoplay = value === 'on';
+      }
+
+      if (key === 'autopause') {
+        obj.autopause = value === 'on';
+      }
+
+      if (key === 'button') {
+        obj.buttons = obj.buttons || [];
+        obj.buttons.push(parseButtonConfig(value, configElement.querySelector(':scope > ul')));
       }
     }
 
@@ -47,6 +84,185 @@ function parseConfig(block) {
   return configObj;
 }
 
+function addCss(styles) {
+  const css = document.createElement('style');
+  css.type = 'text/css';
+  if (css.styleSheet) {
+    css.styleSheet.cssText = styles;
+  } else {
+    css.appendChild(document.createTextNode(styles));
+  }
+
+  document.getElementsByTagName('head')[0].appendChild(css);
+}
+
+function setupCustomPlayButton(block, player, config) {
+  const button = document.createElement('button');
+  button.classList.add('custom-play-button');
+  button.addEventListener('click', () => {
+    if (player.paused()) {
+      player.play();
+    } else {
+      player.pause();
+    }
+  });
+
+  const img = document.createElement('img');
+  img.setAttribute('src', config.svg);
+
+  button.append(img);
+
+  const container = block.querySelector('.video-container');
+  container.append(button);
+
+  let top;
+  let bottom;
+  let left;
+  let right;
+  if (config.position === 'top-right') {
+    top = '10px';
+    right = '10px';
+  } else if (config.position === 'top-left') {
+    top = '10px';
+    left = '10px';
+  } else if (config.position === 'bottom-left') {
+    bottom = '10px';
+    left = '10px';
+  } else {
+    bottom = '10px';
+    right = '10px';
+  }
+
+  // eslint-disable-next-line prefer-template
+  const styles = `
+    .custom-play-button {
+      position: absolute;
+      ${(top ? 'top: ' + top + ';' : '')}
+      ${(bottom ? 'bottom: ' + bottom + ';' : '')}
+      ${(left ? 'left: ' + left + ';' : '')}
+      ${(right ? 'right: ' + right + ';' : '')}
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      background-color: #fff;
+      color: #000;
+      padding: 5px;
+      margin: 0;
+    }
+  `;
+
+  addCss(styles);
+}
+
+function setupCustomMuteButton(block, player, config) {
+  const button = document.createElement('button');
+  button.classList.add('custom-mute-button');
+  button.addEventListener('click', () => {
+    if (player.muted()) {
+      player.muted(false);
+    } else {
+      player.muted(true);
+    }
+  });
+
+  const img = document.createElement('img');
+  img.setAttribute('src', config.svg);
+
+  button.append(img);
+
+  const container = block.querySelector('.video-container');
+  container.append(button);
+
+  let top;
+  let bottom;
+  let left;
+  let right;
+  if (config.position === 'top-right') {
+    top = '10px';
+    right = '10px';
+  } else if (config.position === 'top-left') {
+    top = '10px';
+    left = '10px';
+  } else if (config.position === 'bottom-left') {
+    bottom = '10px';
+    left = '10px';
+  } else {
+    bottom = '10px';
+    right = '10px';
+  }
+
+  const styles = `
+    .custom-mute-button {
+      position: absolute;
+      ${(top ? 'top: ' + top + ';' : '')}
+      ${(bottom ? 'bottom: ' + bottom + ';' : '')}
+      ${(left ? 'left: ' + left + ';' : '')}
+      ${(right ? 'right: ' + right + ';' : '')}
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      background-color: #fff;
+      color: #000;
+      padding: 5px;
+      margin: 0;
+    }
+
+    .custom-play-button + .custom-mute-button {
+      ${(top ? 'top: 80px;' : '')}
+      ${(bottom ? 'bottom: 80px;' : '')}
+    }
+  `;
+
+  addCss(styles);
+}
+
+function setupAutopause(video, player) {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        player.play();
+      } else {
+        player.pause();
+      }
+    });
+  }, {
+    threshold: [0.5, 0],
+  });
+
+  observer.observe(video);
+}
+
+function setupPlayer(block, config) {
+  const videoElement = block.querySelector('video.video-js');
+  // eslint-disable-next-line no-undef
+  const player = videojs(videoElement, {
+    bigPlayButton: false,
+  });
+  player.src({
+    src: config.url,
+    type: 'application/x-mpegURL',
+  });
+
+  player.muted(config.autoplay || config.muted);
+  player.autoplay(config.autoplay);
+
+  if (config.autopause) {
+    setupAutopause(videoElement, player);
+  }
+
+  if (config.buttons && config.buttons.length > 0) {
+    config.buttons.forEach((button) => {
+      if (button.type === 'play') {
+        setupCustomPlayButton(block, player, button);
+      }
+
+      if (button.type === 'mute') {
+        setupCustomMuteButton(block, player, button);
+      }
+    });
+  }
+}
+
 export function isVideo(element) {
   const header = element.querySelector('h2');
   return header && header.textContent.trim().toLowerCase() === 'video';
@@ -55,12 +271,16 @@ export function isVideo(element) {
 export function decorateVideo(block) {
   const config = parseConfig(block);
   const video = document.createElement('video');
-  video.setAttribute('id', 'video-player');
-  video.setAttribute('controls', true);
+  video.setAttribute('controls', config.controls);
   video.classList.add('video-js', 'vjs-default-skin');
 
+  const container = document.createElement('div');
+  container.classList.add('video-container');
+  container.style.position = 'relative';
+  container.append(video);
+
   block.innerHTML = '';
-  block.append(video);
+  block.append(container);
 
   loadVideoJs()
     .then(() => {
@@ -72,13 +292,6 @@ export function decorateVideo(block) {
 
       return Promise.resolve();
     }).then(() => {
-      // eslint-disable-next-line no-undef
-      const player = videojs('video-player');
-      player.src({
-        src: config.url,
-        type: 'application/x-mpegURL',
-      });
-      player.muted(config.autoplay);
-      player.autoplay(config.autoplay);
+      setupPlayer(block, config);
     });
 }

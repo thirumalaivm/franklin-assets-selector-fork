569530ddfe71331af833cbf1b9ea84f582f7c0ac
RitwikSrivastava <45959816+RitwikSrivastava@users.noreply.github.com>
2025-01-07 15:44:46 +0530

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -3,5 +3,7 @@ import { sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
+document.dispatchEvent(new Event('delayed-phase'));
+Window.DELAYED_PHASE = true;
 
 // add more delayed functionality here

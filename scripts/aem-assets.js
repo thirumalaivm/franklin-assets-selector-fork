ae8f882a58a3788ca71b187f98e663ced7a66828
Satya Deep Maheshwari <satyam@adobe.com>
2025-02-10 23:19:36 +0530

    Squashed 'plugins/aem-assets-plugin/' changes from 4e53e53..69d8d4f
    69d8d4f Address review feedback
    10b0be8 Fix indentation
    03890d8 Ensure smartcrops are added for images with delivery url in alt-text
    
    git-subtree-dir: plugins/aem-assets-plugin
    git-subtree-split: 69d8d4fc62a24e04946379a1c37ca479a53be7cf

--- scripts/aem-assets.js
+++ scripts/aem-assets.js
@@ -141,7 +141,7 @@ export function createOptimizedPictureWithSmartcrop(src, alt = '', eager = false
 
   // initialise breakpoint to project level smartcrop config unless needed to customise
   const smartcropBreakpoints = breakpoints.length !== 0 ? breakpoints
-  : Object.entries(window.hlx.aemassets?.smartCrops).map(
+    : Object.entries(window.hlx.aemassets?.smartCrops).map(
       ([name, { minWidth, maxWidth }]) => ({
         media: `(min-width: ${minWidth}px) and (max-width: ${maxWidth}px)`,
         smartcrop: name,
@@ -201,10 +201,10 @@ function hasImageSmartcropMeta() {
 }
 
 /**
- * to filter out all the external images on the page and set data-smartcrop-status=loading
+ * to mark all the external images with smart crop on the page and set data-smartcrop-status=loading
  * if the image is a DM OpenAPI URL
  */
-function filterSmartCropImagesOnPage() {
+function markSmartCropImages(ele = document) {
   // Early return if smartcrop config is missing
   if (window.hlx?.aemassets?.smartCrops === undefined) {
     return;
@@ -215,11 +215,11 @@ function filterSmartCropImagesOnPage() {
 
   if (hasImageSmartcropMeta()) {
     // If smartcrop is enabled at page level, collect all <a> tags
-    extImages.push(...document.querySelectorAll('a'));
+    extImages.push(...ele.querySelectorAll('a'));
   } else {
     // if not enabled at page level, collect all <a> tags within block and section elements
-    extImages.push(...document.querySelectorAll('.smartcrop a'));
-    document.querySelectorAll('.section-metadata > div > div').forEach((sectionMeta) => {
+    extImages.push(...ele.querySelectorAll('.smartcrop a'));
+    ele.querySelectorAll('.section-metadata > div > div').forEach((sectionMeta) => {
       if (sectionMeta.innerText === 'smartcrop') {
         extImages.push(...sectionMeta.closest('.section-metadata').parentElement.querySelectorAll('a'));
       }
@@ -244,7 +244,7 @@ function filterSmartCropImagesOnPage() {
   */
 export function decorateExternalImages(ele, deliveryMarker) {
   // apply data-smartcrop-status=loading to all potential <a> tags
-  filterSmartCropImagesOnPage();
+  markSmartCropImages(ele);
 
   const extImages = ele.querySelectorAll('a');
   extImages.forEach((extImage) => {
@@ -284,10 +284,10 @@ export function decorateExternalImages(ele, deliveryMarker) {
 
 /**
  * Decorates all images in a container element and replace media urls with delivery urls.
- * @param {Element} main The container element
+ * @param {Element} ele The container element
  */
-export function decorateImagesFromAlt(main) {
-  const pictureElements = main.querySelectorAll('picture');
+export function decorateImagesFromAlt(ele = document) {
+  const pictureElements = ele.querySelectorAll('picture');
   [...pictureElements].forEach((pictureElement) => {
     const imgElement = pictureElement.querySelector('img');
     const alt = imgElement.getAttribute('alt');
@@ -298,7 +298,9 @@ export function decorateImagesFromAlt(main) {
         return;
       }
 
-      const newPictureElement = createOptimizedPicture(deliveryUrl, altText);
+      const newPictureElement = isDMOpenAPIUrl(deliveryUrl)
+        ? createOptimizedPictureWithSmartcrop(deliveryUrl, altText)
+        : createOptimizedPicture(deliveryUrl, altText);
       pictureElement.parentElement.replaceChild(newPictureElement, pictureElement);
     } catch (error) {
       // Do nothing
@@ -349,6 +351,7 @@ export async function loadBlock(block) {
 // Create an object with the test functions
 const testFunctions = {
   appendQueryParams,
+  decorateImagesFromAlt,
 };
 
 // Export the object
--- tests/scripts/aem-assets.test.js
+++ tests/scripts/aem-assets.test.js
@@ -33,3 +33,19 @@ describe('appendQueryParams', () => {
     expect(result).toBe('https://example.com/?existing=param&rotate=90');
   });
 });
+
+describe('decorateImagesFromAlt', () => {
+  it('should decorate images from alt', () => {
+    const container = document.createElement('div');
+    container.innerHTML = `
+      <picture>
+        <img src="https://example.com/image.jpg" alt='{"deliveryUrl":"https://delivery.com/image.jpg","altText":"Example Image"}'>
+      </picture>
+    `;
+    const { decorateImagesFromAlt } = testFunctions;
+    decorateImagesFromAlt(container);
+    expect(container.innerHTML).toBe(`
+      <picture><source media="(min-width: 600px)" type="image/webp" srcset="https://delivery.com/image.jpg?width=2000"><source type="image/webp" srcset="https://delivery.com/image.jpg?width=750"><source media="(min-width: 600px)" srcset="https://delivery.com/image.jpg?width=2000"><img loading="lazy" alt="Example Image" src="https://delivery.com/image.jpg?width=750"></picture>
+    `);
+  });
+});
